0ac485ac7c5760dbcb000cbf45879285123e9f90
leanghok120 <leanghokoeng5@gmail.com>
2024-07-07 17:43:43 +0700

    Add workspaces (not working)

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/leanghok120/moody-go
 
 go 1.22.4
 
-require (
-	github.com/jezek/xgb v1.1.1 // indirect
-	github.com/jezek/xgbutil v0.0.0-20231116234834-47f30c120111 // indirect
-)
+require github.com/jezek/xgb v1.1.1
+
+require github.com/jezek/xgbutil v0.0.0-20231116234834-47f30c120111 // indirect
--- main.go
+++ main.go
@@ -6,7 +6,62 @@ import (
 
 	"github.com/jezek/xgb"
 	"github.com/jezek/xgb/xproto"
+	"github.com/jezek/xgbutil"
+	"github.com/jezek/xgbutil/ewmh"
 )
+
+type Workspace struct {
+	windows []xproto.Window
+}
+
+var (
+	workspaces       []Workspace
+	currentWorkspace int
+
+	xu *xgbutil.XUtil
+)
+
+// EWMH
+func initEWMH(xu *xgbutil.XUtil) error {
+	// Set supported EWMH properties
+	supported := []string{
+		"_NET_WM_DESKTOP",
+		"_NET_CURRENT_DESKTOP",
+		"_NET_NUMBER_OF_DESKTOPS",
+	}
+
+	return ewmh.SupportedSet(xu, supported)
+}
+
+func setWindowWorkspace(window xproto.Window, workspace int, xu *xgbutil.XUtil) {
+	ewmh.WmDesktopSet(xu, window, uint(workspace))
+}
+
+// Workspace func
+func initWorkspace(n int) {
+	workspaces = make([]Workspace, n)
+	currentWorkspace = 0
+}
+
+func switchWorkspace(newWorkspace int, conn *xgb.Conn) {
+	if newWorkspace < 0 || newWorkspace >= len(workspaces) {
+		return
+	}
+
+	// Unmap windows in current workspace
+	for _, window := range workspaces[currentWorkspace].windows {
+		xproto.UnmapWindow(conn, window)
+	}
+
+	// Map windows in new workspace
+	for _, window := range workspaces[newWorkspace].windows {
+		xproto.MapWindow(conn, window)
+		setWindowWorkspace(window, newWorkspace, xu)
+	}
+
+	currentWorkspace = newWorkspace
+	ewmh.CurrentDesktopSet(xu, uint(newWorkspace))
+}
 
 func handleConfigureRequest(ev xproto.ConfigureRequestEvent, conn *xgb.Conn) {
 	configureEvent := xproto.ConfigureNotifyEvent{
@@ -27,12 +82,17 @@ func handleConfigureRequest(ev xproto.ConfigureRequestEvent, conn *xgb.Conn) {
 
 // Map window and configure it
 func handleMapRequest(ev xproto.MapRequestEvent, conn *xgb.Conn, screenWidth uint16, screenHeight uint16) (err error) {
+	// Add the window to current workspace
+	workspaces[currentWorkspace].windows = append(workspaces[currentWorkspace].windows, ev.Window)
+	fmt.Println("Add window to current workspace")
+
 	// Map window
 	err = xproto.MapWindowChecked(conn, ev.Window).Check()
 	if err != nil {
 		return err
 	}
 
+	// Resize window to fullscreen
 	values := []uint32{0, 0, uint32(screenWidth), uint32(screenHeight)}
 	var masks uint16 = xproto.ConfigWindowX | xproto.ConfigWindowY | xproto.ConfigWindowWidth | xproto.ConfigWindowHeight
 	xproto.ConfigureWindowChecked(conn, ev.Window, masks, values)
@@ -46,11 +106,27 @@ func handleMapRequest(ev xproto.MapRequestEvent, conn *xgb.Conn, screenWidth uin
 	return nil
 }
 
-func handleKeyPress(ev xproto.KeyPressEvent) {
+func handleKeyPress(ev xproto.KeyPressEvent, conn *xgb.Conn) {
 	fmt.Println(ev.Detail)
-	// 'p'
-	if ev.Detail == 33 {
+	switch ev.Detail {
+	case 65: // space
 		exec.Command("rofi", "-show", "drun").Run()
+	case 24: // q
+		switchWorkspace(0, conn)
+	case 25: // w
+		switchWorkspace(1, conn)
+	case 26: // e
+		switchWorkspace(2, conn)
+	case 27: // r
+		switchWorkspace(3, conn)
+	case 28: // t
+		switchWorkspace(4, conn)
+	case 29: // y
+		switchWorkspace(5, conn)
+	case 30: // u
+		switchWorkspace(6, conn)
+	case 31: // i
+		switchWorkspace(7, conn)
 	}
 }
 
@@ -59,6 +135,13 @@ func main() {
 	conn, err := xgb.NewConn()
 	if err != nil {
 		fmt.Println("Could not open connection")
+		return
+	}
+
+	// xgbutil
+	xu, err = xgbutil.NewConn()
+	if err != nil {
+		fmt.Println("Could not open connnection to xgbutil")
 		return
 	}
 
@@ -71,6 +154,15 @@ func main() {
 
 	screen := connInfo.DefaultScreen(conn)
 	root := screen.Root
+
+	initWorkspace(9)
+
+	// Initialize ewmh
+	err = initEWMH(xu)
+	if err != nil {
+		fmt.Println("Could not initialize EWMH")
+		return
+	}
 
 	// Set attributes for root window
 	mask := []uint32{
@@ -99,7 +191,7 @@ func main() {
 
 		switch event := ev.(type) {
 		case xproto.KeyPressEvent:
-			handleKeyPress(event)
+			handleKeyPress(event, conn)
 		case xproto.ConfigureRequestEvent:
 			handleConfigureRequest(event, conn)
 		case xproto.MapRequestEvent:
